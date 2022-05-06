b6347244d45d0c136567a4b98aa5647fbfe14fe0
admin <admin@CHETUIWK222>
2022-05-06 19:11:03 +0530

    Add project files.

---
+++ Controllers/ashishController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Data_First_aaproach.Controllers
+{
+    public class ashishController : Controller
+    {
+        ADOPPEntities obj = new ADOPPEntities();
+        // GET: ashish
+        public ActionResult Index()
+        {
+            List<Employee> employees = obj.Employees.ToList();
+            return View(employees);
+        }
+    }
+}
